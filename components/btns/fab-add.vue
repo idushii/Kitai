2869565264c1e021d74c53120baf19cfa70a2370
Nicolay <nic343@ngs.ru>
2018-07-31 23:04:58 +0700

    Добавил плавающую кнопку добавить.

---
+++ components/btns/fab-add.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="fab-add">
+    +
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fab-add'
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    border-radius: 2rem; 
+    background-color: rgba(236, 103, 103, 0.5);
+    position: fixed;
+    right: 1rem;
+    bottom: 1rem;
+    width: 1rem;
+    height: 1rem;
+    padding: 1rem;
+    text-align: center;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all .2s;
+    box-shadow: 0rem 0rem .5rem black;
+    &:hover {
+      user-select: none;
+      box-shadow: 0rem 0rem .3rem black;
+    }
+  }
+</style>
