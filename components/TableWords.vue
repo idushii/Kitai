ab8313bb80756ba0fcfd91001ee7497d5bb261d4
Nicolay <nic343@ngs.ru>
2018-08-02 11:48:05 +0700

    Создал компонент

---
+++ components/TableWords.vue
@@ -0,0 +1,42 @@
+<template>
+  <table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Пиньинь</th>
+            <th>Иероглиф</th>
+            <th>Перевод</th>
+            <th>Уровень</th>
+            <th>Категории</th>
+            <th>Звучание</th>
+        </tr>
+    </thead>
+      <transition-group name="word" tag="tbody">
+        <tr v-for="(word, index) in list" :key="`word-${word.Pinyin}`" :key1="`word-${word.Pinyin}`" @click="Open(word)">
+          <td>{{index+1}}</td>
+          <td>{{word.Pinyin}}</td>
+          <td class="word">{{word.Hieroglyph}}</td>
+          <td>{{word.Translate}}</td>
+          <td>{{word.Level}}</td>
+          <td>{{word.tags.map(tag => tag.Title).join(', ')}}</td>
+          <td class="audio"><audio-file :src="word.link" /></td>
+        </tr>
+      </transition-group>
+  </table>
+</template>
+
+<script>
+import audioFile from '~/components/audio-file.vue'
+
+export default {
+  name: 'TableWords',
+  props: {
+    list: { type: Array, default: () => [] },
+    Open: { type: Function, default: () => {} }
+  },
+  components: {
+    audioFile,
+  }
+
+}
+</script>
