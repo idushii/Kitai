4098440de9eba26b9a58dd3d9668733e8dd138fb
Nicolay <nic343@ngs.ru>
2018-08-02 11:47:15 +0700

    Перенес компонент из архива

---
+++ components/audio-file.vue
@@ -0,0 +1,17 @@
+<template>
+  <audio class="audio-file" :src=src controls></audio>
+</template>
+
+<script>
+export default {
+  name: 'audio-plaeeyr',
+  props: {
+    src: { type: String, default: "" }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
