397be90c318b7a329ca89fcc3e202f5998610ffc
Nicolay <nic343@ngs.ru>
2018-08-03 18:17:18 +0700

    Подключил локальные компоненты глобально для всего проекта

--- plugins/global.js
+++ plugins/global.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import card from '../components/card.vue'
 
+import selectTree from '~/components/selectTree.vue'
+import audioFile from '~/components/audio-file.vue'
+import uploadSound from '~/components/upload-sound.vue'
+import SelectCategory from '~/components/SelectCategory.vue'
+
 Vue.component('card', card)
+Vue.component('selectTree', selectTree)
+Vue.component('audioFile', audioFile)
+Vue.component('uploadSound', uploadSound)
+Vue.component('SelectCategory', SelectCategory)
 
 Vue.mixin({
   computed: {
