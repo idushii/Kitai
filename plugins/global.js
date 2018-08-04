dfd184e1445ccaf77f62b8e2410364ba261ecc1c
Nicolay <nic343@ngs.ru>
2018-08-04 07:58:37 +0700

    Подключил компоненты FabAdd, TestEditInfo, EditItem глобально

--- plugins/global.js
+++ plugins/global.js
@@ -5,12 +5,18 @@ import selectTree from '~/components/selectTree.vue'
 import audioFile from '~/components/audio-file.vue'
 import uploadSound from '~/components/upload-sound.vue'
 import SelectCategory from '~/components/SelectCategory.vue'
+import FabAdd from '~/components/btns/fab-add.vue'
+import TestEditInfo from '~/components/Test/EditInfo.vue'
+import EditItem from '~/components/Test/EditItem.vue'
 
 Vue.component('card', card)
 Vue.component('selectTree', selectTree)
 Vue.component('audioFile', audioFile)
 Vue.component('uploadSound', uploadSound)
 Vue.component('SelectCategory', SelectCategory)
+Vue.component('FabAdd', FabAdd)
+Vue.component('TestEditInfo', TestEditInfo)
+Vue.component('EditItem', EditItem)
 
 Vue.mixin({
   computed: {
