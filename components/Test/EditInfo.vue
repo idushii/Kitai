df3d29a54acccbc8011833b20faef897b4992cd2
Nicolay <nic343@ngs.ru>
2018-08-04 07:52:33 +0700

    Добвил возможность исопльзования вместе с компонентом директивы v-model

--- components/Test/EditInfo.vue
+++ components/Test/EditInfo.vue
@@ -2,7 +2,7 @@
   <card class="TestEditInfo" :close=close @close="EventClose">
     <div v-if="title" slot="title">{{title}}</div>
     <div slot="content">
-      <input type="text" v-model="Item.Title">
+      <input type="text" :value="Item.Title" @input="setTitle">
       <select-category :id-category="Item.idCategory" @input="setCategory" />
       <no-ssr placeholder="Редактор загружается...">
         <textarea :id="`EditorSmall`" :value=Item.Info></textarea>
@@ -15,14 +15,13 @@
 </template>
 
 <script>
-import SelectCategory from '~/components/SelectCategory.vue'
-
 export default {
   name: 'TestEditInfo',
   props: {
     id: { type: Number, default: null },
     close: { type: Boolean, default: false },
     title: { type: String, default: "" },
+    Value: { type: [Boolean, Object], default: false }
   },
   data: () => ({
     Item: {},
@@ -30,9 +29,13 @@ export default {
     EditorFull: {},
   }),
   async created() {
-    this.Item = {...this.$store.getters.test(this.id)} || {};
-    if (this.Item.Info === undefined) await this.$store.dispatch('GET_TEST_LIST');
-    this.Item = {...this.$store.getters.test(this.id)};
+    if (this.Value) {
+      this.Item = {...this.Value}
+    } else {
+      this.Item = {...this.$store.getters.test(this.id)} || {};
+      if (this.Item.Info === undefined) await this.$store.dispatch('GET_TEST_LIST');
+      this.Item = {...this.$store.getters.test(this.id)};
+    }
   },
   async mounted() { 
     await this.$nextTick()
@@ -51,7 +54,7 @@ export default {
       images_upload_url: '/api/Img/Upload',
       images_upload_base_path: '/img',
       init_instance_callback: function (editor) {
-        editor.on('change', function (e) { thisEditor.Item.Info = tinymce.get('EditorSmall').getContent() });
+        editor.on('change', function (e) { thisEditor.Item.Info = tinymce.get('EditorSmall').getContent(); thisEditor.EventInput() });
       }
     });
 
@@ -68,22 +71,23 @@ export default {
       images_upload_url: '/api/Img/Upload',
       images_upload_base_path: '/img',
       init_instance_callback: function (editor) {
-        editor.on('change', function (e) { thisEditor.Item.Info = tinymce.get('EditorFull').getContent() });
+        editor.on('change', function (e) { thisEditor.Item.InfoFull = tinymce.get('EditorFull').getContent(); thisEditor.EventInput() });
       }
     });
   },
   beforeDestroy() { tinymce.remove() },
   methods: {
-    setCategory(id) { this.Item.idCategory = id },
+    setCategory(id) { this.Item.idCategory = id; this.EventInput(); },
+    setTitle(e) { this.Item.Title = e.target.value; this.EventInput(); },
     EventClose() {
       if (this.close) this.$emit('close', this.Item)
       if (this.save) this.save(this.Item)
       return this.Item
+    },
+    EventInput() {
+      this.$emit('input', this.Item)
     }
   },
-  components: {
-    SelectCategory
-  }
 }
 </script>
 
