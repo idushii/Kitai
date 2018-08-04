f0db6ae59580ca8d4e01fd8311fa58c6296a7ae1
Nicolay <nic343@ngs.ru>
2018-08-04 07:54:03 +0700

    Компонент для создания нового теста

---
+++ pages/Test/New.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <card clings title back="/Test" :save=save>Создать тест</card>
+    <test-edit-info v-model="Test" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewTest',
+  layout: 'document',
+  data: () => ({
+    Test: {
+      id: null,
+      idCategory: 3,
+      Title: '',
+      Info: '',
+      InfoFull: '',
+    }
+  }),
+  methods: {
+    async save() {
+      let result = await this.$store.dispatch('NEW_TEST', this.Test)
+      if (result) {
+        this.$router.push(`/Test/${this.$store.getters.TestLastID}/Edit`)
+      } else {
+        alert(result.message)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
