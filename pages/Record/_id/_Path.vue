595b14493e15da4315b1b197aca2a60274450722
idushii <nic34@ngs.ru>
2018-08-10 09:13:17 +0700

    Страница просмотра записи

---
+++ pages/Record/_id/_Path.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <card title :back="`/Page/${Page.Path}/${Category.Path}`" :edit="isAuth" :edit-link="`/Record/${id}/Edit`">{{Record.Title}}</card>
+    <card>
+      <div id="content-record" v-html="Record.Text" />
+    </card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Record',
+  layout: 'site',
+  computed: {
+    id() { return this.$route.params.id },
+    Record() {
+      return this.$store.getters['Record'](this.id)
+    },
+    Page() {
+      return this.$store.getters['PageById'](this.Record.idPage)
+    },
+    Category() {
+      return this.$store.getters['CategoryById'](this.Record.idCategory)
+    }
+  }
+}
+</script>
