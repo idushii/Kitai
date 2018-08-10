1c8ffe7757e5b04faa9d134d842bed42b4ebd0f1
idushii <nic34@ngs.ru>
2018-08-10 13:31:00 +0700

    ++

--- api/connection.js
+++ api/connection.js
@@ -1,13 +1,6 @@
 import mysql from 'mysql'
 
 let mysqlCofig = {
-  host     : 'mysql.j693917.myjino.ru',
-  user     : 'j693917',
-  password : '7654321',
-  database: 'j693917_sayana'
-}
-
-mysqlCofig = {
   host     : 'localhost',
   user     : 'root',
   password : '',
