a4835baed3ec0b66e5a4a3a1aaf4f66f8d2e4b86
idushii <nic34@ngs.ru>
2018-08-17 14:51:36 +0700

    ++

--- components/menu-admin.vue
+++ components/menu-admin.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="admin-menu" class="menu">
-    <div class="title">Управление, {{isUser ? isUser.username : ''}}</div>
-    <template v-if="isUser">
+    <div class="title">Управление, {{isAuth ? $store.getters.USER.Login : ''}}</div>
+    <template v-if="isAuth">
       <nuxt-link :to="`/Edit/${$route.params.Path}`"> Изменить </nuxt-link>
       <nuxt-link to="/propertis"> Настройки </nuxt-link>
+      <nuxt-link to="/profile"> Профиль </nuxt-link>
       <nuxt-link to="/logout"> Выйти </nuxt-link>
     </template>
     <template v-else>
       <nuxt-link to="/auth"> Авторизация </nuxt-link>
+      <nuxt-link to="/reg"> Регистрация </nuxt-link>
     <nuxt-link to="/propertis"> Настройки </nuxt-link>
     </template>
   </div>
@@ -16,7 +18,6 @@
 export default {
   name: 'admin-menu',
   computed: {
-    isUser() { return this.$store.getters.USER }
   }
 }
 </script>
