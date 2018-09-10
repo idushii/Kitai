682f2eeb6ae86fec7531a0e4c31d81b3ef457680
Nicolay I <nic.nic343@ya.ru>
2018-09-10 07:35:36 +0700

    Добавил сокрытие ненужных категорий

--- components/menu.vue
+++ components/menu.vue
@@ -4,7 +4,15 @@
     <template v-for="page in items" >
       <nuxt-link :key="`page-${page.id}`" :to="`/Page/${page.Path}`"> {{page.Title}} </nuxt-link>
       <template v-if="page.Categoris">
-        <nuxt-link v-for="Cat in page.Categoris" :key="`page-${page.id}-category-${Cat.id}`" :to="`/Page/${page.Path}/${Cat.Path}`" class="category"> {{Cat.Title}} </nuxt-link>
+        <nuxt-link 
+          v-for="Cat in page.Categoris" 
+          :key="`page-${page.id}-category-${Cat.id}`" 
+          v-if="!Cat.isHide"
+          :to="`/Page/${page.Path}/${Cat.Path}`" 
+          class="category"
+        > 
+          {{Cat.Title}} 
+        </nuxt-link>
       </template>
     </template>
   </div>
