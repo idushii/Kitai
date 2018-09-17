32370a51ecc63ed912009a45cf7f2ad6da73b911
Nicolay I <nic.nic343@ya.ru>
2018-09-17 19:43:07 +0700

    ++

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -112,7 +112,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"baseUrl":"http://localhost:3000/","host":"http://localhost:3000/"}
+      env: {"baseUrl":"http://localhost:3000/","host":"http://ovz1.j693917.1qyn6.vps.myjino.ru/"}
     }
     // Only set once
     if (context.req) app.context.req = context.req
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser'
 import session from 'express-session'
 
 let host = 'http://localhost:3000/';
-//host = 'http://ovz1.j693917.1qyn6.vps.myjino.ru/';
+host = 'http://ovz1.j693917.1qyn6.vps.myjino.ru/';
 
 export default {
   head: {
